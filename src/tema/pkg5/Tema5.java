a00956ea1a27963128a90ba2e2b9a9c465d3f4f0
EduardoGarcia32 <slender560@gmail.com>
2019-02-18 07:52:34 -0600

    Calcular la hipotenusa

---
+++ src/tema/pkg5/Tema5.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema.pkg5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author AlumnoUTM
+ */
+public class Tema5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);
+        double ladoA, ladoB, hipotenusa;
+        System.out.println("Ingrese valor del lado A");
+        ladoA = sc.nextDouble();
+        System.out.println("Ingrese valor del lado B");
+        ladoB = sc.nextDouble();
+        hipotenusa = CalcularHipotenusa(ladoA,ladoB);
+               
+    }
+    
+    public static doblue CalcularHipotenusa 
+    
+}
