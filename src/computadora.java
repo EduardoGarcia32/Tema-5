ac7a54de6fcae062c21e367126e4865abb8ddd03
EduardoGarcia32 <slender560@gmail.com>
2019-02-18 07:55:52 -0600

    Nueva clase

---
+++ src/computadora.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AlumnoUTM
+ */
+public class computadora {
+    private int valor;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
+    
+}
